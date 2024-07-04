adb7b1f22bb7868b117e7fc73ebd1451ff0d50bd
Destin Nguyen <thanhdat09122001@gmail.com>
2024-07-04 17:42:50 +0700

    init(ui): Init project with basic ui

---
+++ src/app/(auth)/login/components/user-auth-form.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import * as React from "react";
+import { z } from "zod";
+import { cn } from "@/lib/utils";
+import { useForm } from "react-hook-form";
+
+import { Icons } from "@/components/icons";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/shadcn/ui/form";
+import { Button } from "@/shadcn/ui/button";
+import { Input } from "@/shadcn/ui/input";
+import { toast } from "sonner";
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const FormSchema = z.object({
+    email: z.string().min(5, {
+        message: "Username must be at least 2 characters.",
+    }),
+    password: z.string().min(3, {
+        message: "Username must be at least 2 characters.",
+    }),
+});
+
+export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+    const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+
+    function onSubmit(data: z.infer<typeof FormSchema>) {
+        toast("You submitted the following values:", {
+            description: (
+                <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+                    <code className="text-white">
+                        {JSON.stringify(data, null, 2)}
+                    </code>
+                </pre>
+            ),
+        });
+
+        setIsLoading(true);
+
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 3000);
+    }
+
+    return (
+        <div className={cn("grid gap-6", className)} {...props}>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                    <div className="grid gap-2">
+                        <div className="grid gap-1">
+                            <FormField
+                                control={form.control}
+                                name="email"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                id="email"
+                                                placeholder="email@chubb.com"
+                                                type="email"
+                                                autoCapitalize="none"
+                                                autoComplete="email"
+                                                autoCorrect="off"
+                                                disabled={isLoading}
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            id="password"
+                                            placeholder="password"
+                                            type="password"
+                                            autoCapitalize="none"
+                                            disabled={isLoading}
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit" disabled={isLoading}>
+                            {isLoading && (
+                                <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                            )}
+                            Sign In
+                        </Button>
+                    </div>
+                </form>
+            </Form>
+        </div>
+    );
+}
---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+    name: "CHUBB Portal",
+    url: "https://fiora.vn",
+    ogImage: "https://fiora.vn/og.jpg",
+    description:
+        "Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
+    links: {
+        twitter: "https://#",
+        github: "https://#",
+    },
+};
+
+export type SiteConfig = typeof siteConfig;
